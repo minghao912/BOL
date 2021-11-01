5e84c06cbf871439b5a8075e0483fcb56123f3dd
minghao912 <minghaochen912@gmail.com>
2021-10-31 17:29:57 -0700

    OAuth Response stays in storage after refresh

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { RouteChildrenProps } from 'react-router';
+
+import { GlobalContext } from "../context/GlobalState";
 
 interface RouteParams {
     username: string
 }
 
-export class ProfilePage extends React.Component<RouteChildrenProps> {
-    render() {
-        return (<>
-            <p>Profile Page</p>
-            <p>Your username is {(this.props.match!.params as RouteParams).username}</p>
-        </>);
-    }
+export function ProfilePage(props: any): JSX.Element {
+    const { OAuthResponse } = useContext(GlobalContext);
+
+    useEffect(() => {
+        console.log(OAuthResponse);
+    }, [OAuthResponse])
+
+    return (<>
+        <p>Profile Page</p>
+        <p>Your username is {(props.match!.params as RouteParams).username}</p>
+    </>);
 }
 
 export default ProfilePage;
