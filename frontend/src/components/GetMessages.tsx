6dc08aa47f802bc4e085fb2c342d83df7fd963ae
minghao912 <minghaochen912@gmail.com>
2021-11-07 14:49:19 -0800

    Add seperate route for debug

---
+++ frontend/src/components/GetMessages.tsx
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+export default function GetMessages(props: any): JSX.Element {
+    return <p>Test</p>
+}
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 
-// import Homepage from './pages/Homepage';
+import Homepage from './pages/Homepage';
 import DebugHomepage from './pages/DebugHomepage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
@@ -11,7 +11,8 @@ function AppRouter() {
     return (
     <Switch>
         <Route path="/" exact component={ LoginPage } />
-        <Route path="/home" exact render={(props) => <DebugHomepage {...props}/>} />
+        <Route path="/home" exact render={(props) => <Homepage {...props}/>} />
+        <Route path="/debug" exact render={(props) => <DebugHomepage {...props}/>} />
         <Route path="/profile/:googleID" render={(props) =>  <ProfilePage {...props} hasID={true} /> } />
         <Route path="/profile" render={(props) => <ProfilePage {...props} hasID={false} /> } />
         {/*<Route path="/profile/:username" component={ ProfilePage } />*/}        
