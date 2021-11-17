0cc8582d1ab105b66bc2150231ce970c7ede7134
Prateik11 <prateiksinha@g.ucla.edu>
2021-11-17 13:29:57 -0800

    "Add Friend" button fixed, "Home" button added
    "Add Friend" appears when you are on someone elses profile,"Home" appears
    when you are on your own profile

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -46,12 +46,15 @@ export function ProfilePage (props: ProfilepageProps, param: RouteParams){
             setImageUrl(userObj.profilePicPath)
             setName(userObj.username)
             setProfileID(userObj.userID)
-        }).catch(err => console.error(err));
+        }).catch(err => {
+            //console.log("Reached error block");
+            //console.error(err);
+            return (<Redirect to ={"/profile/" + username} />);
+            });
     }, [OAuthResponse]);
 
-    // console.log(s);
+    console.log(imageUrl);
     // console.log(props.match.params.googleID);
-
     // console.log("Reached redirect statement 1");
     // console.log(OAuthResponse.profileObj.googleId);
     // console.log(newGoogleID);
@@ -69,7 +72,35 @@ export function ProfilePage (props: ProfilepageProps, param: RouteParams){
     
     if(newGoogleID != profileID)
     {
-        
+        return (
+            <div className="profileContainer">
+                <div className="profileBox"> 
+                    <div className="profileSpace_top"></div>
+                    <div className="profilelogoContainer" > 
+                        <img src={BOL} alt= 'BOL logo' width = "360" height = "153.3"/>
+                    </div>
+                    <div className="profile_space_Between_Logo_and_UserProfile">
+                        <p><b>User Profile</b></p>
+                    </div>
+                    <div className="profileSpaceSmall"></div>
+                    <div className="profilelogoContainer" style={{color: COLORS.FULL_WHITE}}>
+                        <img id='123' src= {imageUrl} alt = "Profile Picture" width = "150" height = "150" />
+                    </div>
+                    <div className="profileBreak">
+                        <p> {name} </p>
+                    </div>
+                    <div className="profileSpace_middle"></div>  
+                    <div className="profileBreak">
+                        {/* <p> Hi! I am {(props.match!.params as RouteParams).googleID} and I love BOL! </p> */}
+                        <p> Hi! I am {name} and I love BOL! </p>
+                    </div>
+                    <div className="profileSpace_bottom"></div>
+                    <div className="profilefriendButton">
+                    <span style={{color:"#ffffff"}} >Add Friend </span>
+                    </div>
+                </div>
+            </div>
+        );
     }
     
     return (
@@ -96,7 +127,7 @@ export function ProfilePage (props: ProfilepageProps, param: RouteParams){
                 </div>
                 <div className="profileSpace_bottom"></div>
                 <div className="profilefriendButton">
-                <span style={{color:"#ffffff"}} >Add Friend </span>
+                <span style={{color:"#ffffff"}} >Home </span>
                 </div>
             </div>
         </div>
