b41a7d13fd2a1dbc273aaffdd22021f098f24261
Mike <mikequmrsh@gmail.com>
2021-11-24 15:47:38 -0800

    user-searching front end integration
    the search page now returns json articles of user matches in the console

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,6 +5,7 @@ import DebugHomepage from './pages/DebugHomepage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import PageNotFoundPage from './pages/PageNotFoundPage';
+import SearchPage from './pages/SearchPage';
 
 // Handles which URLs go to which pages
 function AppRouter() {
@@ -15,6 +16,7 @@ function AppRouter() {
         <Route path="/debug" exact render={(props) => <DebugHomepage {...props}/>} />
         <Route path="/profile/:googleID" render={(props) =>  <ProfilePage {...props} hasID={true} /> } />
         <Route path="/profile" render={(props) => <ProfilePage {...props} hasID={false} /> } />
+        <Route path="/search" exact component={ SearchPage } />
         {/*<Route path="/profile/:username" component={ ProfilePage } />*/}        
 
         {/* This is the default case */}
