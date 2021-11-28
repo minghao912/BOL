c7e715967d6453d8b4ce3d4e0fa213292c7f0917
Prateik11 <prateiksinha@g.ucla.edu>
2021-11-28 15:35:11 -0800

    Edit Profile now opens popup to enter new bio

--- frontend/src/components/Popup.tsx
+++ frontend/src/components/Popup.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Popup.css'
+import { TextField } from '@mui/material';
 
 interface PopupProps{
     trigger: boolean
@@ -15,6 +16,18 @@ export function Popup(props: PopupProps){
             </div>
             <div className="popupText">
             <span style={{color:"#ffffff"}} > Enter new Bio: </span>
+            </div>
+            <div  className="text">
+                <TextField
+                id="first-name"
+                label="User Bio"
+                margin= "normal"
+                // value={this.state.name}
+                // onChange={this.handleChange('name')}
+                />
+            </div>
+            <div className="submit" onClick = {() => props.setTrigger(false)}>
+            <span style={{color:"#ffffff"}} > Submit </span>
             </div>
         </div>
     ) : <></>;
